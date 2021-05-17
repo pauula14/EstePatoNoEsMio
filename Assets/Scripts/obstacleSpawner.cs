fe09f3e0fa72944e1bf2f4c59576ad8f8efdde35
Prothoky <Prothoky@github.com>
2021-05-17 13:11:21 +0200

    Random Spawn Obstacles

---
+++ Assets/Scripts/obstacleSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obstacleSpawner : MonoBehaviour
+{
+    public GameObject[] obstaclePrefab;
+    
+    private void Start() {
+            int rand = Random.Range(0, obstaclePrefab.Length);
+            int[] randomRot = {0,90,180,270};
+            Instantiate(obstaclePrefab[rand], new Vector3(transform.position.x, 0, transform.position.z), Quaternion.Euler(0,randomRot[Random.Range(0,randomRot.Length)],0));        
+    }
+
+
+
+}
